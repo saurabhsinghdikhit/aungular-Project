bfd050bad6a5380ff920af38ec82df68d610dfeb
saurabh.singh <ss520553@gmail.com>
2021-04-15 13:08:39 +0530

    updated

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component,EventEmitter, Output } from "@angular/core";
+
+@Component({
+    templateUrl:'./header.component.html',
+    selector:'app-header'
+})
+export class HeaderComponent{
+   @Output() featureSelected =new EventEmitter<string>();
+    onSelect(feature:string){
+        this.featureSelected.emit(feature);
+    }
+}
---
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../Models/recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test','very good','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636'),
+    new Recipe('Test2','very good','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
